b9e3483d4ff7af80de0d6d94a5ef6bb61aef8e16
Cxetive <54766299+Cxetive@users.noreply.github.com>
2024-04-17 14:00:32 +0200

    Update

---
+++ src/database.php
@@ -0,0 +1,44 @@
+<?php
+require_once('../config/config.php');
+
+class Database
+{
+    Private $connectie;
+
+    public function __construct() {
+        $this->connectie = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    }
+
+    public function voerQueryUit($query)
+    {
+        $result = $this->connectie->query($query);
+        $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+    }
+
+    public function sluitVerbinding()
+    {
+        $this->connectie = null;
+    }
+
+    public function __destruct()
+    {
+        $this->sluitVerbinding();
+    }
+}
+
+    function testVerbinding()
+    {
+        if($connectie = true)
+        {
+            echo "<br>Er is een actieve verbinding<br>";
+        }
+        else 
+        {
+            echo "<br>Er is geen actieve verbinding<br>";
+        }
+    }
+
+$mijn_database = new Database();
+
+?>
