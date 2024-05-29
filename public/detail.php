470e4f3df462546513d8e900d7f55ae01349e2bf
Boet <boet.reijrink@student.graafschapcollege.nl>
2024-05-29 14:41:48 +0200

    Detail pagina

--- public/detail.php
+++ public/detail.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/detail.css">
     <title>Document</title>
 </head>
 
@@ -17,12 +18,14 @@
     }
 
     ?>
-
+<div class="container">
+    <a href="index.php">Terug</a>
+    <div class="box">
     <h1>Klant: <?php echo $customerData[0]['voornaam'] . " " . $customerData[0]['achternaam']; ?></h1>
     <p>Email: <?php echo $customerData[0]['email']; ?></p>
     <p>Klus: <?php echo $customerData[0]['klus']; ?></p>
-    <a href="update.php?customerID=<?php echo $customerData[0]['customerId']; ?>">Bewerken</a></br>
-    <a href="delete.php?customerID=<?php echo $customerData[0]['customerId']; ?>">Verwijderen</a>
+    </div>
+    </div>
 
 </body>
 
