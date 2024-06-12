007704ee6b30a5649a31e34a5fde1345ee69576c
Lasse <lasse.smeman@student.graafschapcollege.nl>
2024-06-12 13:49:48 +0200

    formulier gemaakt

---
+++ public/factuur.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Factuur maken</title>
+</head>
+<body>
+    
+        <form method="get">
+            <input type="text" name="verzender" id="" placeholder="verzender">
+            <input type="text" name="ontvanger" id="" placeholder="ontvanger">
+            <input type="text" name="bedrag" id="" placeholder="bedrag">
+            <input type="text" name="klus" id="" placeholder="klus">
+            <input type="submit" value="verzenden" name="verzenden" >
+        </form>
+    
+</body>
+</html>
