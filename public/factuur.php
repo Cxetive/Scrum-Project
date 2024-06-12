6745b393ddbec5e5f7a26151a5b49aad6eb02725
Lasse <lasse.smeman@student.graafschapcollege.nl>
2024-06-12 14:09:48 +0200

    frontend factuur

--- public/factuur.php
+++ public/factuur.php
@@ -4,16 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Factuur maken</title>
+    <link rel="stylesheet" href="styles/factuur.css">
 </head>
 <body>
-    
+    <div class="container">
+        <h1>Factuur aanmaken</h1>
         <form method="get">
             <input type="text" name="verzender" id="" placeholder="verzender">
             <input type="text" name="ontvanger" id="" placeholder="ontvanger">
             <input type="text" name="bedrag" id="" placeholder="bedrag">
             <input type="text" name="klus" id="" placeholder="klus">
-            <input type="submit" value="verzenden" name="verzenden" >
+            <input type="submit" value="Verzenden" name="verzenden" >
         </form>
+    </div>
+    
     
 </body>
 </html>
